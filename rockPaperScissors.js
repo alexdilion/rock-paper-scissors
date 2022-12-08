60d02abc62ed9f5ada4b5f0d7401aa7ecace5243
alexdilion <alexdilion@protonmail.com>
2022-12-08 19:12:51 +0000

    Add playRound function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,13 +1,46 @@
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
-  
+
   switch (randomNumber) {
     case 0:
-      return 'Rock';
+      return "Rock";
     case 1:
-      return 'Scissors';
+      return "Paper";
     case 2:
-      return 'Paper';
+      return "Scissors";
   }
 }
- 
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  if (playerSelection != computerSelection) {
+    switch (playerSelection) {
+      case "rock":
+        if (computerSelection == "scissors") {
+          return "You win! Rock beats scissors.";
+        } else {
+          return "You lose! Paper beats rock.";
+        }
+      case "paper":
+        if (computerSelection == "rock") {
+          return "You win! Paper beats rock.";
+        } else {
+          return "You lose! Scissors beats paper.";
+        }
+      case "scissors":
+        if (computerSelection == "paper") {
+          return "You win! Scissors beats paper.";
+        } else {
+          return "You lose! Rock beats scissors.";
+        }
+      default:
+        alert("Enter a valid selection!");
+        break;
+    }
+  } else {
+    return("Tie!");
+  }
+}
+
