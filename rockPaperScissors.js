83d6773809d0fa2b296b49615ff088a31c8c9368
alexdilion <alexdilion@protonmail.com>
2022-12-08 18:48:08 +0000

    Add getComputerChoice function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,13 @@
-console.log("Everything's working");
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  
+  switch (randomNumber) {
+    case 0:
+      return 'Rock';
+    case 1:
+      return 'Scissors';
+    case 2:
+      return 'Paper';
+  }
+}
+ 
