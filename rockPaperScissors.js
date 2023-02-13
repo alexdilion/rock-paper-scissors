e70c1959965891d535a63b684e2523c266797260
alexdilion <alexdilion@protonmail.com>
2023-02-13 23:30:11 +0000

    Finish everything :D

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,11 +1,14 @@
+// Object with all possible outcomes
 const roundOutcomes = {
     rock: { rock: "tie", paper: "loss", scissors: "win" },
     paper: { rock: "win", paper: "tie", scissors: "loss" },
     scissors: { rock: "loss", paper: "win", scissors: "tie" },
 };
 
+// Input buttons
 const buttons = [...document.querySelectorAll(".rps-button")];
 
+// Elements on the page
 let gameState = document.querySelector("#game-state");
 let playerScoreCounter = document.querySelector("#player-score");
 let computerScoreCounter = document.querySelector("#computer-score");
@@ -13,12 +16,13 @@ let turnHistory = document.querySelector("#turn-history");
 let playerImage = document.querySelector("#player-image");
 let computerImage = document.querySelector("#computer-image");
 
+// Holds the current turn li, null if there are no li in turnHistory (this is for the blue highlighting)
 let currentTurnElement = null;
 
 let playerWins = 0;
 let computerWins = 0;
 
-// randomly generate a computer selection
+// Get computer selection
 function getComputerSelection() {
     const choices = ["rock", "paper", "scissors"];
     // generate a random number between 1 and 3
@@ -27,17 +31,19 @@ function getComputerSelection() {
     return choices[randomNumber];
 }
 
-// update game state, win counters and output turn outcome to history
+// Update game state and win counters and output turn outcome to history
 function displayResult(outcome, playerSelection) {
     gameState.textContent = `You ${outcome}!`;
 
     playerScoreCounter.textContent = playerWins;
     computerScoreCounter.textContent = computerWins;
 
+	// Takes off id for highlighting on the previous li
     if (currentTurnElement !== null) {
         currentTurnElement.removeAttribute("id");
     }
 
+	// New li element
     let newElement = document.createElement("li");
     newElement.id = "current-turn";
     newElement.textContent = `You chose ${playerSelection} and ${outcome}`;
@@ -46,8 +52,10 @@ function displayResult(outcome, playerSelection) {
     turnHistory.appendChild(newElement);
 }
 
+// Update images for computer and player selections
 function updateImages(playerSelection, computerSelection) {
-    if (playerImage.classList.contains("transparent")) {
+    // I set the images to transparent in the restart function, I set them back to opaque here
+	if (playerImage.classList.contains("transparent")) {
         playerImage.classList.remove("transparent");
         computerImage.classList.remove("transparent");
     }
@@ -56,6 +64,7 @@ function updateImages(playerSelection, computerSelection) {
     computerImage.src = `./images/${computerSelection}.png`;
 }
 
+// Reset all relevant variables and text and what not
 function restart() {
     computerWins = 0;
     playerWins = 0;
@@ -67,12 +76,13 @@ function restart() {
     playerImage.classList.add("transparent");
     computerImage.classList.add("transparent");
 
+	// Empty turnHistory ul
     turnHistory.innerHTML = "";
 
-	turnHistory.classList = "empty-ul";
+    turnHistory.classList = "empty-ul";
 }
 
-// play a round
+// Play a round
 function playRound(playerSelection) {
     playerSelection = playerSelection.toLowerCase();
     let computerSelection = getComputerSelection();
@@ -92,23 +102,35 @@ function playRound(playerSelection) {
     }
 }
 
+// Ask player if they want to play again
+function promptPlayAgain() {
+    let answer = confirm("Do you want to play again?");
+
+    if (answer) {
+        restart();
+    }
+}
+
+// Run game logic after an input is given
 function onButtonClicked(e) {
     const button = e.target;
     let playerSelection = button.id;
 
-	if (turnHistory.hasChildNodes && turnHistory.classList == "empty-ul") {
-		turnHistory.classList = "non-empty-ul";
-	}
+    if (turnHistory.hasChildNodes && turnHistory.classList == "empty-ul") {
+        turnHistory.classList = "non-empty-ul";
+    }
 
-    if (playerWins < 5 && computerWins < 5) {
+    if (playerWins !== 5 && computerWins !== 5) {
         playRound(playerSelection);
     } else {
-        let answer = prompt("Do you want to play again? (y/n)");
-
-        if (answer === "y") {
-			restart();
-        }
-    }
+		if (playerWins === 5) {
+			alert("You beat the computer! :D");
+			promptPlayAgain();
+		} else if (computerWins === 5) {
+			alert("You lost! :(");
+			promptPlayAgain();
+		}
+	}
 }
 
 buttons.forEach((button) => {
